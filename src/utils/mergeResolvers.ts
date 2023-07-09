2938eba0ae5122447d21480d9ed9d4440e91cbc7
jankuca <jan@jankuca.com>
2023-07-09 17:00:39 +0200

    fix resolver merging to not emit empty root types

--- src/utils/mergeResolvers.ts
+++ src/utils/mergeResolvers.ts
@@ -6,30 +6,55 @@ export function mergeResolvers<S extends AnySchemaType, T extends object[]>(para
   resolvers: T
 }): MergePartialSchemaResolvers<T[number]> {
   const { Schema, resolvers: modules } = params
-  return Object.assign({}, ...modules, {
-    [Schema['Query']['typename']]: Object.assign(
-      {},
-      ...modules.map((m) =>
-        Schema['Query']['typename'] in m // @ts-expect-error The key has been checked for existence, it is there.
-          ? m[Schema['Query']['typename']]
-          : {}
-      )
-    ),
-    [Schema['Mutation']['typename']]: Object.assign(
-      {},
-      ...modules.map((m) =>
-        Schema['Mutation']['typename'] in m // @ts-expect-error The key has been checked for existence, it is there.
-          ? m[Schema['Mutation']['typename']]
-          : {}
-      )
-    ),
-    [Schema['Subscription']['typename']]: Object.assign(
-      {},
-      ...modules.map((m) =>
-        Schema['Subscription']['typename'] in m // @ts-expect-error The key has been checked for existence, it is there.
-          ? m[Schema['Subscription']['typename']]
-          : {}
-      )
-    ),
-  })
+
+  const queryTypename = Schema['Query']['typename']
+  const mutationTypename = Schema['Mutation']['typename']
+  const subscriptionTypename = Schema['Subscription']['typename']
+
+  const queryResolvers = modules.reduce(
+    (merged, partial) =>
+      queryTypename in partial
+        ? Object.assign(
+            merged ?? {},
+            // @ts-expect-error The key is there.
+            partial[queryTypename]
+          )
+        : merged,
+    null
+  )
+  const mutationResolvers = modules.reduce(
+    (merged, partial) =>
+      mutationTypename in partial
+        ? Object.assign(
+            merged ?? {},
+            // @ts-expect-error The key is there.
+            partial[mutationTypename]
+          )
+        : merged,
+    null
+  )
+  const subscriptionResolvers = modules.reduce(
+    (merged, partial) =>
+      subscriptionTypename in partial
+        ? Object.assign(
+            merged ?? {},
+            // @ts-expect-error The key is there.
+            partial[subscriptionTypename]
+          )
+        : merged,
+    null
+  )
+
+  const mergedResolvers = Object.assign({}, ...modules)
+  if (queryResolvers) {
+    mergedResolvers[queryTypename] = queryResolvers
+  }
+  if (mutationResolvers) {
+    mergedResolvers[mutationTypename] = mutationResolvers
+  }
+  if (subscriptionResolvers) {
+    mergedResolvers[subscriptionTypename] = subscriptionResolvers
+  }
+
+  return mergedResolvers
 }
