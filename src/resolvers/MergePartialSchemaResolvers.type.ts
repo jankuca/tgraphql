a7c9e3622fb83438bfd7c123d568c7d60efac238
jankuca <jan@jankuca.com>
2023-12-27 16:46:21 +0100

    simplify schema handling in MergePartialSchemaResolvers

--- src/resolvers/MergePartialSchemaResolvers.type.ts
+++ src/resolvers/MergePartialSchemaResolvers.type.ts
@@ -1,4 +1,4 @@
-import { AnySchemaType, EmptySchemaType, SchemaType } from '../SchemaType'
+import { AnySchemaType, EmptySchemaType } from '../SchemaType'
 import { ObjectUnionToObjectIntersection } from '../types/ObjectUnionToObjectIntersection.type'
 
 type FilterByKey<U, K extends string> = Extract<U, { [key in K]: any }>
@@ -7,22 +7,27 @@ export type MergePartialSchemaResolvers<
   // WARN: We must not use `extends PartialSchemaResolvers` here as it would not allow using
   //   `satisfies PartialSchemaResolvers` for resolver implemention with exact inferrence.
   Rs extends object,
-  S extends AnySchemaType = EmptySchemaType
-> = S extends SchemaType<infer Q, infer M, infer S>
-  ? Omit<ObjectUnionToObjectIntersection<Rs>, Q['typename'] | M['typename'] | S['typename']> &
-      ([FilterByKey<Rs, Q['typename']>] extends [never]
-        ? Record<never, any>
-        : {
-            [typename in Q['typename']]: ObjectUnionToObjectIntersection<FilterByKey<Rs, typename>[typename]>
-          }) &
-      ([FilterByKey<Rs, M['typename']>] extends [never]
-        ? Record<never, any>
-        : {
-            [typename in M['typename']]: ObjectUnionToObjectIntersection<FilterByKey<Rs, typename>[typename]>
-          }) &
-      ([FilterByKey<Rs, S['typename']>] extends [never]
-        ? Record<never, any>
-        : {
-            [typename in S['typename']]: ObjectUnionToObjectIntersection<FilterByKey<Rs, typename>[typename]>
-          })
-  : never
+  Schema extends AnySchemaType = EmptySchemaType
+> = Omit<
+  ObjectUnionToObjectIntersection<Rs>,
+  Schema['Query']['typename'] | Schema['Mutation']['typename'] | Schema['Subscription']['typename']
+> &
+  ([FilterByKey<Rs, Schema['Query']['typename']>] extends [never]
+    ? Record<never, any>
+    : {
+        [typename in Schema['Query']['typename']]: ObjectUnionToObjectIntersection<FilterByKey<Rs, typename>[typename]>
+      }) &
+  ([FilterByKey<Rs, Schema['Mutation']['typename']>] extends [never]
+    ? Record<never, any>
+    : {
+        [typename in Schema['Mutation']['typename']]: ObjectUnionToObjectIntersection<
+          FilterByKey<Rs, typename>[typename]
+        >
+      }) &
+  ([FilterByKey<Rs, Schema['Subscription']['typename']>] extends [never]
+    ? Record<never, any>
+    : {
+        [typename in Schema['Subscription']['typename']]: ObjectUnionToObjectIntersection<
+          FilterByKey<Rs, typename>[typename]
+        >
+      })
