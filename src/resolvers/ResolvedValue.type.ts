30668238a49c65cc939e2236553f336953574c06
jankuca <jan@jankuca.com>
2024-01-01 18:11:12 +0100

    fix entity object usage in deeper levels of resolver return objects

--- src/resolvers/ResolvedValue.type.ts
+++ src/resolvers/ResolvedValue.type.ts
@@ -1,16 +1,27 @@
 import { AnyObjectType, ObjectType } from '../outputs/ObjectType'
+import { AnyParamObjectType } from '../outputs/ParamObjectType'
 import { UnionType } from '../outputs/UnionType'
 import { AnyType } from '../types/AnyType.type'
+import { Prettify } from '../types/Prettify.type'
 import { Value } from '../types/Value.type'
+
+type ResolvedObjectValue<
+  S extends Record<string, { type: AnyType; optional: boolean; params: AnyParamObjectType | null }>,
+  Entities extends { [typename in string]?: object }
+> = Prettify<{
+  [key in keyof S]: S[key]['optional'] extends true
+    ? ResolvedValue<S[key]['type'], Entities> | null
+    : ResolvedValue<S[key]['type'], Entities>
+}>
 
 export type ResolvedValue<T extends AnyType, Entities extends { [typename in string]?: object }> = T extends ObjectType<
   infer N,
-  any
+  infer S
 >
   ? N extends keyof Entities
     ? // Defined entity objects are expected to be returned by resolvers instead of fully resolved objects.
       Entities[N]
-    : Value<T>
+    : ResolvedObjectValue<S, Entities>
   : T extends UnionType<any, infer I extends ReadonlyArray<AnyObjectType>>
   ? ResolvedValue<I[number], Entities>
   : T extends [infer I extends AnyType, null]
